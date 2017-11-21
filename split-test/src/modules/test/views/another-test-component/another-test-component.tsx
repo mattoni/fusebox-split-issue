e8a9a82964bc3622812156f4608bfe11f74a5aa6
Mattoni <amattoni@petrichor.io>
2017-11-20 16:02:18 -0800

    initial commit

---
+++ split-test/src/modules/test/views/another-test-component/another-test-component.tsx
@@ -0,0 +1,5 @@
+import { loadContentComponent } from "../../../../common/ui/layou";
+export function AnotherTestComponent() {
+  console.log("I am another test component");
+  loadContentComponent();
+}
